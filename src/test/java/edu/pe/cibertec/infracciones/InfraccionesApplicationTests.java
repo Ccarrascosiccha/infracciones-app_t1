b636122186f11352a4cd03df9f75eab7a696e828
Ccarrasco <ccarrascosiccha@gmail.com>
2026-04-18 01:17:58 -0500

    InfraccionesApplicationTest fixed :)

--- src/test/java/edu/pe/cibertec/infracciones/InfraccionesApplicationTests.java
+++ src/test/java/edu/pe/cibertec/infracciones/InfraccionesApplicationTests.java
@@ -1,10 +1,34 @@
 package edu.pe.cibertec.infracciones;
 
+import edu.pe.cibertec.infracciones.repository.InfractorRepository;
+import edu.pe.cibertec.infracciones.repository.MultaRepository;
+import edu.pe.cibertec.infracciones.repository.PagoRepository;
+import edu.pe.cibertec.infracciones.repository.TipoInfraccionRepository;
+import edu.pe.cibertec.infracciones.repository.VehiculoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration",
+		"spring.sql.init.mode=never"
+})
 class InfraccionesApplicationTests {
+
+	@MockitoBean
+	InfractorRepository infractorRepository;
+
+	@MockitoBean
+	VehiculoRepository vehiculoRepository;
+
+	@MockitoBean
+	MultaRepository multaRepository;
+
+	@MockitoBean
+	PagoRepository pagoRepository;
+
+	@MockitoBean
+	TipoInfraccionRepository tipoInfraccionRepository;
 
 	@Test
 	void contextLoads() {
